19bb6009cf6bfb0b88422a0489271d6d05c262cb
Almaz Fazulzyanov <afazulzyanovg@gmail.com>
2024-11-16 23:43:47 +0400

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+//! # Random String Generator
+//! This library provides a function to generate a random string of a given length.
+
 use rand::Rng;
 
+/// This function generates a random string of a given length
+/// 
+/// Example:
+/// ```
+/// let random_string = get_random_string(10);
+/// ```
 pub fn get_random_string(length: i32) -> String {
     let chars: Vec<char> = (33..127).map(|i| i as u8 as char).collect();
     let mut rng = rand::thread_rng();
